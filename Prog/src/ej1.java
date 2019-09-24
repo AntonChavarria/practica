a15a064b43b6256569629619315219d0496779cb
anton.chavarria <anton.chavarria@E110L38.opendeusto.es>
2019-09-24 08:41:16 +0200

    Creada estructura del Proyecto

---
+++ Prog/src/ej1.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+
+public class ej1 extends JFrame{
+	
+	public static final long serialVersionUID = 1L;
+	
+	
+	public ej1() {
+	
+		this.setTitle("vENTANA PARA EXIT");
+		this.setSize(300,300);
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		//this.setVisible(true);
+		}
+	public static void main(String[] args) {
+		ej1 mainWindow = new ej1();
+		mainWindow.setVisible(true);
+	}
+}
